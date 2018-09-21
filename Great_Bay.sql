15e0e6e443297c43947bc6ebf9c535e615d2feee
jrbreckenitch <jamesbreckenitch@stp-er-10-129-52-181.wireless.umn.edu>
2018-09-20 20:46:13 -0500

    created database

---
+++ Great_Bay.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS Great_BayDB;
+
+CREATE DATABASE Great_BayDB;
+
+USE Great_BayDB;
+
+CREATE TABLE Post_Item (
+  id INT NOT NULL AUTO_INCREMENT,
+  item_name VARCHAR(255) NULL,
+  minimum_bid INT(255) NULL,
+  accept VARCHAR(255) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Bid_Item (
+  id INT NOT NULL AUTO_INCREMENT,
+  item_name VARCHAR(255) NULL,
+  bid INT(255) NULL,
+  too_low VARCHAR(255) NULL,
+  current_bid VARCHAR(255) NULL,
+  PRIMARY KEY (id)
+);
+
+
+
