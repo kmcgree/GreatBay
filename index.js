5d4d49daac182aebef8a06311302ad73c6830b26
Kane <kmcgree14@gmail.com>
2018-09-20 21:00:43 -0500

    init

---
+++ index.js
@@ -0,0 +1,46 @@
+const inquire = require('inquirer');
+const mysql = require('mysql');
+
+const start = () => {
+    inquire.prompt(
+        [
+            {
+                type: "list",
+                name: "choice",
+                message: "Would like to POST and item or BID on an item?",
+                choices: ["Post item","Bid on an item"]
+            }
+        ]
+    ).then((res) => {
+        console.log('༼ つ ◕ ◕ ༽つ Sending Energy ༼ つ ◕ ◕ ༽つ ' + res.choice + ' ༼ つ ◕ ◕ ༽つ');
+        if (res.choice === "Post item") {
+            postItem();
+        }else {
+            bidItem();
+        }
+    })
+};
+
+const postItem = () => {
+    inquire.prompt([
+        {
+            type: 'input',
+            name: "item",
+            message: "What item are you posting"
+        },
+        {
+            type: 'input',
+            name: 'price',
+            message: 'Enter a starting price'
+        }
+    ]).then((res) => {
+        let value = [];
+        value.push(res.item);
+        value.push(res.price);
+
+        postDB(value);
+    })
+};
+
+const 
+start()
